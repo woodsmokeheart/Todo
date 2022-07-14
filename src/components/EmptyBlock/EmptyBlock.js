dbf199fb1ff5134c808bc937b8418007c8c94e29
Denis Kukobin <denis.kukobin@hipasus.com>
2022-07-14 20:20:51 +0300

    added img for empty block

--- src/components/EmptyBlock/EmptyBlock.js
+++ src/components/EmptyBlock/EmptyBlock.js
@@ -4,7 +4,12 @@ import { useTranslation } from "react-i18next";
 
 function EmptyBlock() {
   const { t } = useTranslation();
-  return <p className={css.text}>{t("Write your first task")}</p>;
+  return (
+    <div className={css.wrapper}>
+      <img className={css.img} src="/images/list.png" alt="language" />
+      <p className={css.text}>{t("Write your first task")}</p>
+    </div>
+  );
 }
 
 export default EmptyBlock;
